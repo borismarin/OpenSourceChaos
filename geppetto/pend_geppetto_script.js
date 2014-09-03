8c8feb9628b6a574bb55a33005a30a5c11c1f639
Boris Marin <borismarin@gmail.com>
2014-09-03 18:53:12 +0100

    Update pend_geppetto_script.js
    fixed sloppy copy/pasting from HH example: only one graph, bright func points to theta

--- geppetto/pend_geppetto_script.js
+++ geppetto/pend_geppetto_script.js
@@ -6,11 +6,8 @@ Simulation.start();
 //Adding Plot 1
 
 G.addWidget(Widgets.PLOT);
-
 Plot1.setName("Simple pendulum");
-
 options = {yaxis:{min:-10,max:10},xaxis:{min:0,max:400,show:false}};
-
 Plot1.setOptions(options);
 Plot1.setPosition(93,88);
 Plot1.setSize(230,510);
@@ -18,18 +15,15 @@ Plot1.plotData("pend.electrical.SimulationTree.theta");
 
 G.wait(100);
 
-G.addWidget(Widgets.PLOT);
-
-options = {yaxis:{min:0,max:250},xaxis:{min:0,max:400,show:false}};
-
-
+//Adding silly info
 G.addWidget(Widgets.POPUP);
 Popup1.setMessage("I am testing Geppetto functionality with generic LEMS models.");
 Popup1.setName("Description");
 Popup1.setPosition(710,398);
 Popup1.setSize(160,498);
 
-window.setTimeout(function(){Simulation.addBrightnessFunction("pend.electrical", "pend.electrical.SimulationTree.x", function(x){return (x+0.06)/0.06;})},500);
+//Adding sphere colour modulation
+window.setTimeout(function(){Simulation.addBrightnessFunction("pend.electrical", "pend.electrical.SimulationTree.theta", function(x){return (x+0.06)/0.06;})},500);
 
 G.incrementCameraZoom(-0.15);
 
