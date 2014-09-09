84dd261f1fd9c320f617cdfe03ff43a1658b3c6b
Matteo Cantarelli <matteo.cantarelli@gmail.com>
2014-09-09 12:06:01 -0400

    Update scrg_silly.js

--- geppetto/scrg_silly.js
+++ geppetto/scrg_silly.js
@@ -9,7 +9,7 @@ options = {yaxis:{min:0,max:200},xaxis:{min:0,max:100,show:false}};
 Plot1.setOptions(options);
 Plot1.setPosition(93,88);
 Plot1.setSize(230,510);
-Plot1.plotData("car_entity.electrical.SimulationTree.x");
+Plot1.plotData(car_entity.electrical.SimulationTree.x);
 
 //Adding silly info
 G.addWidget(Widgets.POPUP);
