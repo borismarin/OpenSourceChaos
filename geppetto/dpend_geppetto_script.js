0b1dd5ce65d5d7813f9a08443e84dd42c39a4947
Boris Marin <borismarin@gmail.com>
2014-10-03 20:47:15 +0100

    s/pend/dpend/ in rest of script, theta0/1->1/2

--- geppetto/dpend_geppetto_script.js
+++ geppetto/dpend_geppetto_script.js
@@ -1,4 +1,4 @@
-Simulation.addWatchLists([{name:"pendvars",variablePaths:["dpend.mechanical.SimulationTree.theta1", "dpend.mechanical.SimulationTree.p1","dpend.mechanical.SimulationTree.theta2", "dpend.mechanical.SimulationTree.p2"]}]);
+Simulation.addWatchLists([{name:"dpendvars",variablePaths:["dpend.mechanical.SimulationTree.theta1", "dpend.mechanical.SimulationTree.p1","dpend.mechanical.SimulationTree.theta2", "dpend.mechanical.SimulationTree.p2"]}]);
 Simulation.startWatch();
 Simulation.start();
 
@@ -9,7 +9,7 @@ options = {yaxis:{min:-2,max:2},xaxis:{min:0,max:100,show:false}};
 Plot1.setOptions(options);
 Plot1.setPosition(93,88);
 Plot1.setSize(230,510);
-Plot1.plotData(pend.mechanical.SimulationTree.theta1);
+Plot1.plotData(dpend.mechanical.SimulationTree.theta1);
 
 G.wait(100);
 
@@ -19,7 +19,7 @@ Plot2.setPosition(93,339);
 Plot2.setSize(230,510);
 Plot2.setName("angle arm2");
 Plot2.setOptions(options);
-Plot2.plotData(pend.mechanical.SimulationTree.theta2);
+Plot2.plotData(dpend.mechanical.SimulationTree.theta2);
 
 
 //Adding silly info
@@ -29,5 +29,5 @@ Popup1.setName("Description");
 Popup1.setPosition(710,398);
 Popup1.setSize(160,498);
 
-Simulation.addDynamicVisualization(pend.visual, "pivot0", pend.mechanical.SimulationTree.theta0, GEPPETTO.setLocalRotationZ, function(x){return x});
-Simulation.addDynamicVisualization(pend.visual, "pivot1", pend.mechanical.SimulationTree.theta1, GEPPETTO.setLocalRotationZ, function(x){return x});
+Simulation.addDynamicVisualization(dpend.visual, "pivot0", dpend.mechanical.SimulationTree.theta1, GEPPETTO.setLocalRotationZ, function(x){return x});
+Simulation.addDynamicVisualization(dpend.visual, "pivot1", dpend.mechanical.SimulationTree.theta2, GEPPETTO.setLocalRotationZ, function(x){return x});
